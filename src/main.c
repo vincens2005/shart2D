0b2c9c7ed7598e986c03c6a3399231d2f1cfbe40
cukmekerb <cukmekerb@gmail.com>
2022-09-03 15:25:24 -0700

    0,0 in center of screen

--- src/main.c
+++ src/main.c
@@ -3,14 +3,14 @@
 #include "include/objects.h"
 
 int main() {
-
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(640, 480, "physics moment!!!");
 	SetTargetFPS(60); // 60 fps
 
 	physicsBall b = {
-	.base.weight = 1,
-	.radius = 20,
-	.base.position = {GetScreenWidth()/2.0f, GetScreenHeight()/2.0f}
+		.base.weight = 1,
+		.radius = 20,
+		.base.position = {0, 0}
 	};
 
 	while (!WindowShouldClose()) {
@@ -21,7 +21,9 @@ int main() {
 
 		ClearBackground(BLACK);
 
-		DrawCircleV(b.base.position, (float)b.radius, RED);
+		Vector2 offset = {GetScreenWidth()/2.0f, GetScreenHeight()/2.0f};
+
+		DrawCircleV((Vector2){b.base.position.x + offset.x, b.base.position.y + offset.y}, (float)b.radius, RED);
 
 		DrawFPS(10, 10); // show current fps on screen
 
